6cbc4d2288cfe07e627d5c87ac47bac8e7ac1a28
igorferreir4 <94236607+igorferreir4@users.noreply.github.com>
2022-11-26 00:00:15 -0300

    new

---
+++ scripts/caddybuild-win64.sh
@@ -0,0 +1,5 @@
+xcaddy build --output ./caddy.exe \
+    --with github.com/caddy-dns/cloudflare \
+    --with github.com/mholt/caddy-l4 \
+    --with github.com/RussellLuo/caddy-ext/layer4 \
+    --with github.com/caddyserver/cache-handler
