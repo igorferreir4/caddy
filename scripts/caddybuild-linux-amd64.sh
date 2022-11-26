9a2fc47778053c09714f088441b5cb7f8de45b7a
igorferreir4 <94236607+igorferreir4@users.noreply.github.com>
2022-11-26 00:53:55 -0300

    update

--- scripts/caddybuild-linux-amd64.sh
+++ scripts/caddybuild-linux-amd64.sh
@@ -1,4 +1,4 @@
-GOOS=linux GOARCH=amd64 xcaddy build --output ./caddy-amd64 \
+GOOS=linux GOARCH=amd64 xcaddy build --output ./caddy-linux-amd64 \
     --with github.com/caddy-dns/cloudflare \
     --with github.com/mholt/caddy-l4 \
     --with github.com/RussellLuo/caddy-ext/layer4 \
--- scripts/caddybuild-linux-arm64.sh
+++ scripts/caddybuild-linux-arm64.sh
@@ -1,4 +1,4 @@
-GOOS=linux GOARCH=arm64 xcaddy build --output ./caddy-arm64 \
+GOOS=linux GOARCH=arm64 xcaddy build --output ./caddy-linux-arm64 \
     --with github.com/caddy-dns/cloudflare \
     --with github.com/mholt/caddy-l4 \
     --with github.com/RussellLuo/caddy-ext/layer4 \
