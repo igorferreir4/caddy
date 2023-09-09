04632b0dc3572e9538bd699ad31bd950850fac13
Igor Ferreira <94236607+igorferreir4@users.noreply.github.com>
2023-09-09 16:12:52 -0300

    add module
    add module: github.com/lucaslorentz/caddy-docker-proxy/v2

--- scripts/caddybuild-linux-arm64.sh
+++ scripts/caddybuild-linux-arm64.sh
@@ -2,4 +2,5 @@ GOOS=linux GOARCH=arm64 xcaddy build --output ./caddy-linux-arm64 \
     --with github.com/caddy-dns/cloudflare \
     --with github.com/mholt/caddy-l4 \
     --with github.com/RussellLuo/caddy-ext/layer4 \
-    --with github.com/WeidiDeng/caddy-cloudflare-ip
+    --with github.com/WeidiDeng/caddy-cloudflare-ip \
+    --with github.com/lucaslorentz/caddy-docker-proxy/v2
