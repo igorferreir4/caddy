6b652f0f6c6d7c45372d7f9ba807937032e370a9
Igor Ferreira <94236607+igorferreir4@users.noreply.github.com>
2023-12-14 18:04:45 -0300

    teste

--- scripts/caddybuild-win-arm64.sh
+++ scripts/caddybuild-win-arm64.sh
@@ -2,4 +2,5 @@ GOOS=windows GOARCH=arm64 xcaddy build --output ./caddy-windows-arm64.exe \
     --with github.com/caddy-dns/cloudflare \
     --with github.com/mholt/caddy-l4 \
     --with github.com/RussellLuo/caddy-ext/layer4 \
-    --with github.com/WeidiDeng/caddy-cloudflare-ip
+    --with github.com/WeidiDeng/caddy-cloudflare-ip \
+    --with github.com/lucaslorentz/caddy-docker-proxy/v2
