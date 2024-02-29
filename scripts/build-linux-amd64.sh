105c97fb868dffb4d1aebd21d074f61c2ae49efd
Igor Ferreira <94236607+igorferreir4@users.noreply.github.com>
2024-02-29 02:56:11 -0300

    NEW

--- scripts/build-linux-amd64.sh
+++ scripts/build-linux-amd64.sh
@@ -2,5 +2,4 @@ GOOS=linux GOARCH=amd64 xcaddy build --output ./caddy-linux-amd64 \
     --with github.com/caddy-dns/cloudflare \
     --with github.com/mholt/caddy-l4 \
     --with github.com/RussellLuo/caddy-ext/layer4 \
-    --with github.com/WeidiDeng/caddy-cloudflare-ip \
-    --with github.com/lucaslorentz/caddy-docker-proxy/v2
+    --with github.com/WeidiDeng/caddy-cloudflare-ip
